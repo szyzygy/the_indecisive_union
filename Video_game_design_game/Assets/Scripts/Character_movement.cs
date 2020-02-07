413ac7ca84d120ca36e63f47943ad08ea718bf79
szyzygy <alden.towler@student.nmt.edu>
2020-02-07 15:48:03 -0700

    added first assests

---
+++ Video_game_design_game/Assets/Scripts/Character_movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_movement : MonoBehaviour
+{
+
+
+    CharacterController cr;
+    public float move_speed;
+    public bool move_lock;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cr = GetComponent<CharacterController>();
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!move_lock)
+            Move();
+
+
+
+
+    }
+
+
+    private void Move()
+    {
+
+        float horizontal_in = Input.GetAxis("Horizontal") * move_speed;
+        float vertical_in = Input.GetAxis("Vertical") * move_speed;
+
+        Vector3 move_forward = transform.forward * vertical_in;
+        Vector3 move_sideways = transform.right * horizontal_in;
+
+        cr.SimpleMove(move_forward + move_sideways);
+
+
+    }
+
+
+}
