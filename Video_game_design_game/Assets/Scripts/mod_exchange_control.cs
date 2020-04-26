134110b947c4b37cac6391652b6c4c5d947f8ca2
szyzygy <alden.towler@student.nmt.edu>
2020-04-25 19:42:08 -0600

    added mod exchange capability

---
+++ Video_game_design_game/Assets/Scripts/mod_exchange_control.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mod_exchange_control : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject gun_parent;
+    public List<GameObject> gun_components;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void exchange_mod(UnityEngine.UI.Image mod, int comp) {
+
+        gun_components[comp].GetComponent<UnityEngine.UI.Image>().sprite = mod.sprite;
+
+
+
+    }
+
+
+    private void OnMouseDown()
+    {
+        exchange_mod(this.GetComponent<UnityEngine.UI.Image>(), 0);
+    }
+
+    //*
+    public void OnCollision(Collision collision)
+    {
+
+        if (collision.gameObject == player)
+        {
+            exchange_mod(this.GetComponent<UnityEngine.UI.Image>(), 0);
+        }
+    }
+    //*/
+}
