ddd79a469929ccc2113cced1877bb04c4ac84431
nick41991 <48606329+nick41991@users.noreply.github.com>
2020-04-28 23:15:21 -0600

    Game Monitor Script and Object

---
+++ Video_game_design_game/Assets/Scripts/GameMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMonitor : MonoBehaviour
+{
+
+    float startTime;
+    bool spawnBoss;
+ 
+    void Start()
+    {
+        startTime = Time.time;
+        spawnBoss = false;
+    }
+
+    void Update()
+    {
+        // Set canDoThis to true after 20 seconds have passed
+        if (!spawnBoss  && Time.time - startTime > 80){
+            spawnBoss = true;
+            Debug.Log("Boss is Spawning");
+        }
+    }
+}
