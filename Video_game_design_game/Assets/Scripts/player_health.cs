2cce7080dea32fe8fe200195b7398092d83dad36
nick41991 <48606329+nick41991@users.noreply.github.com>
2020-04-28 22:14:52 -0600

    Player health base logic
    Need to write code for what happens on death

---
+++ Video_game_design_game/Assets/Scripts/player_health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_health : MonoBehaviour
+{
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTrigger(Collider other)
+    {
+        if(other.gameObject.tag == "enemy")
+        {
+            take_damage();
+        }
+    }
+
+    void take_damage()
+    {
+        health -= 1;
+        if(health <= 0)
+        {
+            cause_death();
+        }
+    }
+
+    void cause_death()
+    {
+        //Code for what happens on death
+        //Frowny face- restart button?
+    }
+}
