2579ef214cd6af3d6ee5b4c79d5510b09b1e8819
nick41991 <48606329+nick41991@users.noreply.github.com>
2020-04-28 20:13:37 -0600

    push to pull

---
+++ Video_game_design_game/Assets/Scripts/mod_properties.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mod_properties : MonoBehaviour
+{
+
+    public float strayFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
